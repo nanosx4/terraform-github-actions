6ad8004356992b2b9f0c408ca1e4309a897f83d1
Mateusz Marczuk <marczuk@live.it>
2021-07-29 12:33:46 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+#The configuration for the `remote` backend.
+terraform {
+       backend "remote" {
+         organization="mateusz"
+         workspaces {
+           name = "terraform-github-actions"
+         }
+       }
+     }
+
+     # An example resource that does nothing.
+     resource "null_resource" "example" {
+       triggers = {
+         value = "A example resource that does nothing!"
+       }
+     }
